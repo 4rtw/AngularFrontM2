3ac477616e335e14e3631d027facd36212c09abd
Ando Lalaina <59050819+4rtw@users.noreply.github.com>
2021-03-19 14:30:22 +0300

    Add feature drag and drop Breaks infinite scroll

--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { filter, map, pairwise, throttleTime } from 'rxjs/operators';
 import { AssignmentsService } from '../shared/assignments.service';
 import { Assignment } from './assignment.model';
-import {CdkDrag, CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-assignments',
@@ -13,6 +13,8 @@ import {CdkDrag, CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular
 })
 export class AssignmentsComponent implements OnInit {
   assignments: Assignment[];
+  assignmentsRendus: Assignment[] = [];
+  assignmentsNonRendus: Assignment[] = [];
   page = 1;
   limit = 100;
   totalDocs: number;
@@ -22,7 +24,8 @@ export class AssignmentsComponent implements OnInit {
   hasNextPage: boolean;
   nextPage: number;
 
-  @ViewChild('scroller') scroller: CdkVirtualScrollViewport;
+  @ViewChild('scrollerRendu') scrollerRendu: CdkVirtualScrollViewport;
+  @ViewChild('scrollerNonRendu') scrollerNonRendu: CdkVirtualScrollViewport;
 
   // on injecte le service de gestion des assignments
   constructor(
@@ -59,6 +62,8 @@ export class AssignmentsComponent implements OnInit {
         this.hasNextPage = data.data.hasNextPage;
         this.nextPage = data.data.nextPage;
         console.log('données reçues');
+        this.assignmentsNonRendus = this.assignments.filter( item => item.rendu === false);
+        this.assignmentsRendus = this.assignments.filter( item => item.rendu === true);
       });
   }
 
@@ -79,7 +84,35 @@ export class AssignmentsComponent implements OnInit {
         this.hasNextPage = data.data.hasNextPage;
         this.nextPage = data.data.nextPage;
         console.log('données reçues');
+        this.assignmentsNonRendus = this.assignments.filter( item => item.rendu === false);
+        this.assignmentsRendus = this.assignments.filter( item => item.rendu === true);
       });
+  }
+
+  onScrollList(scroller: CdkVirtualScrollViewport): void{
+    scroller
+        .elementScrolled()
+        .pipe(
+            map((event) => {
+                return scroller.measureScrollOffset('bottom');
+            }),
+            pairwise(),
+            filter(([y1, y2]) => y2 < y1 && y2 < 200),
+            throttleTime(200) // on ne va en fait envoyer le dernier événement que toutes les 200ms.
+            // on va ignorer tous les évéments arrivés et ne garder que le dernier toutes
+            // les 200ms
+        )
+        .subscribe((dist) => {
+        this.ngZone.run(() => {
+            if (this.hasNextPage) {
+                this.page = this.nextPage;
+                console.log(
+                    'Je charge de nouveaux assignments page = ' + this.page
+                );
+                this.getPlusDAssignmentsPourScrolling();
+            }
+        });
+    });
   }
 
   ngAfterViewInit(): void {
@@ -87,38 +120,8 @@ export class AssignmentsComponent implements OnInit {
     // et affiché et ne vaudra pas "undefined" (ce qui aurait été le cas dans ngOnInit)
 
     // On va s'abonner aux évenements de scroll sur le scrolling...
-    this.scroller
-      .elementScrolled()
-      .pipe(
-        map((event) => {
-          return this.scroller.measureScrollOffset('bottom');
-        }),
-        pairwise(),
-        /*
-        tap(([y1, y2]) => {
-          if(y2 < y1) {
-            console.log("ON SCROLLE VERS LE BAS !")
-          } else {
-            console.log("ON SCROLLE VERS LE HAUT !")
-          }
-        }),
-        */
-        filter(([y1, y2]) => y2 < y1 && y2 < 200),
-        throttleTime(200) // on ne va en fait envoyer le dernier événement que toutes les 200ms.
-        // on va ignorer tous les évéments arrivés et ne garder que le dernier toutes
-        // les 200ms
-      )
-      .subscribe((dist) => {
-        this.ngZone.run(() => {
-          if (this.hasNextPage) {
-            this.page = this.nextPage;
-            console.log(
-              'Je charge de nouveaux assignments page = ' + this.page
-            );
-            this.getPlusDAssignmentsPourScrolling();
-          }
-        });
-      });
+      this.onScrollList(this.scrollerRendu);
+      this.onScrollList(this.scrollerNonRendu);
   }
 
   onDeleteAssignment(event): void {
@@ -130,17 +133,7 @@ export class AssignmentsComponent implements OnInit {
     });
   }
 
-    /** Predicate function that only allows even numbers to be dropped into a list. */
-    nonRenduPredicate(assignment: CdkDrag<Assignment>): boolean{
-        return !assignment.data.rendu;
-    }
-
-    /** Predicate function that doesn't allow items to be dropped into a list. */
-    noReturnPredicate(): boolean{
-        return false;
-    }
-
-    drop = (event: CdkDragDrop<Assignment[], any>) => {
+    drop(event: CdkDragDrop<Assignment[], any>): void {
         if (event.previousContainer === event.container) {
             moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
         } else {
