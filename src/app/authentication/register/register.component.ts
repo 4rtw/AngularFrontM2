c363d79c7ecbac53bc0bc7858f12dfe27d189d5a
Ando Lalaina <59050819+4rtw@users.noreply.github.com>
2021-03-21 09:03:13 +0300

    Register -> OK -> TODO improvement

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {UsersService} from '../../shared/users.service';
+import {Users} from '../user.model';
 
 @Component({
   selector: 'app-register',
@@ -6,14 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  hide: boolean;
+  hide = true;
+  nom = '';
+  username = '';
+  password = '';
 
-  constructor() { }
+  constructor(
+      private router: Router,
+      private usersService: UsersService
+  ) { }
 
   ngOnInit(): void {
   }
 
-  onSubmit($event: Event): void {
+  onSubmit(event): void {
+    if ((!this.username) || (!this.password) || (!this.nom)){return; }
 
+    const nouvelUser = new Users();
+    nouvelUser.nom = this.nom;
+    nouvelUser.motDePasse = this.password;
+    nouvelUser.utilisateur = this.username;
+    nouvelUser.isAdmin = false;
+
+    this.usersService.addUser(nouvelUser)
+        .subscribe( reponse => {
+          console.log(reponse.message);
+        });
   }
 }
