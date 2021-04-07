93a26dc2aba9ba930756aa8c00ebcb29f71c1c40
Ando Lalaina <59050819+4rtw@users.noreply.github.com>
2021-04-07 18:03:10 +0300

    Improve update assignment -> Add snackbar on error (+ code alignment)

--- src/app/assignments/edit-assigment/edit-assigment.component.ts
+++ src/app/assignments/edit-assigment/edit-assigment.component.ts
@@ -1,74 +1,84 @@
-import { Component, OnInit } from '@angular/core'
-import { ActivatedRoute, Router } from '@angular/router'
-import { AssignmentsService } from 'src/app/shared/services/assignments.service'
-import { Assignment } from '../../shared/models/assignment.model'
-import {MatSnackBar} from '@angular/material/snack-bar'
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AssignmentsService} from 'src/app/shared/services/assignments.service';
+import {Assignment} from '../../shared/models/assignment.model';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
-  selector: 'app-edit-assigment',
-  templateUrl: './edit-assigment.component.html',
-  styleUrls: ['./edit-assigment.component.css']
+    selector: 'app-edit-assigment',
+    templateUrl: './edit-assigment.component.html',
+    styleUrls: ['./edit-assigment.component.css']
 })
 export class EditAssigmentComponent implements OnInit {
-  assignment: Assignment
+    assignment: Assignment;
 
-  // pour le formulaire
-  nom = ''
-  dateDeRendu = null
+    // pour le formulaire
+    nom = '';
+    dateDeRendu = null;
 
-  constructor(
-    private assignmentsService: AssignmentsService,
-    private route: ActivatedRoute,
-    private router: Router,
-    private snackBar: MatSnackBar
-  ) {}
+    constructor(
+        private assignmentsService: AssignmentsService,
+        private route: ActivatedRoute,
+        private router: Router,
+        private snackBar: MatSnackBar
+    ) {
+    }
 
-  ngOnInit(): void {
-    // ici on montre comment on peut récupérer les parametres http
-    // par ex de :
-    // http://localhost:4200/assignment/1/edit?nom=Michel%20Buffa&metier=Professeur&responsable=MIAGE#edition
+    ngOnInit(): void {
+        // ici on montre comment on peut récupérer les parametres http
+        // par ex de :
+        // http://localhost:4200/assignment/1/edit?nom=Michel%20Buffa&metier=Professeur&responsable=MIAGE#edition
 
-    console.log(this.route.snapshot.queryParams)
-    console.log(this.route.snapshot.fragment)
+        console.log(this.route.snapshot.queryParams);
+        console.log(this.route.snapshot.fragment);
 
-    this.getAssignmentById()
-  }
+        this.getAssignmentById();
+    }
 
-  getAssignmentById(): void {
-    // les params sont des string, on va forcer la conversion
-    // en number en mettant un "+" devant
-    const id: number = +this.route.snapshot.params.id
+    getAssignmentById(): void {
+        // les params sont des string, on va forcer la conversion
+        // en number en mettant un "+" devant
+        const id: number = +this.route.snapshot.params.id;
 
-    console.log('Dans ngOnInit de details, id = ' + id)
-    this.assignmentsService.getAssignment(id).subscribe((assignment) => {
-      this.assignment = assignment
+        console.log('Dans ngOnInit de details, id = ' + id);
+        this.assignmentsService.getAssignment(id).subscribe((assignment) => {
+            this.assignment = assignment;
+            this.nom = assignment.nom;
+            this.dateDeRendu = assignment.dateDeRendu;
+        });
+    }
 
-      this.nom = assignment.nom
-      this.dateDeRendu = assignment.dateDeRendu
-    })
-  }
 
+    onSubmit(event): void {
+        // on va modifier l'assignment
+        if ((!this.nom) || (!this.dateDeRendu)) {
+            return;
+        }
 
-  onSubmit(event): void {
-    // on va modifier l'assignment
-    if ((!this.nom) || (!this.dateDeRendu)) { return }
+        this.assignment.nom = this.nom;
+        this.assignment.dateDeRendu = this.dateDeRendu;
+        const oldName = this.assignment.nom;
 
-    this.assignment.nom = this.nom
-    this.assignment.dateDeRendu = this.dateDeRendu
-    const oldName = this.assignment.nom
+        this.assignmentsService.updateAssignment(this.assignment)
+            .subscribe(message => {
+                    console.log(message.message);
+                    // on affiche une notification
+                    this.snackBar.open(oldName + ' a été modifié avec succès en ' + this.assignment.nom, 'OK', {
+                        duration: 2000
+                    });
+                },
+                error => {
+                    console.log(error.message);
+                    // on affiche une notification
+                    this.snackBar.open('La modification de ' + oldName + ' a échoué ', 'OK', {
+                        duration: 2000, panelClass: ['mat-error']
+                    });
+                },
+                () => {
+                    // et on navigue vers la page d'accueil
+                    this.router.navigate(['/home']);
+                }
+            );
 
-    this.assignmentsService.updateAssignment(this.assignment)
-      .subscribe(message => {
-        console.log(message)
-
-        // et on navigue vers la page d'accueil
-        this.router.navigate(['/home'])
-
-        // on affiche une notification
-        this.snackBar.open(oldName + ' a été modifié avec succès en ' + this.assignment.nom, 'OK', {
-          duration: 2000,
-        })
-      })
-
-  }
+    }
 }
