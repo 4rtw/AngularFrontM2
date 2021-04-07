64a2f1bd79bd7c18d95a1da34dcae86ce1b14ada
Ando Lalaina <59050819+4rtw@users.noreply.github.com>
2021-04-07 18:32:43 +0300

    Improve login,add snackbar on error (+ code alignment)

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -1,42 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../shared/services/auth.service';
 import {Router} from '@angular/router';
-import { Subscription } from 'rxjs';
-import { OnDestroy } from '@angular/core';
+import {Subscription} from 'rxjs';
+import {OnDestroy} from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements  OnInit, OnDestroy{
-  hide = true;
-  login_sub: Subscription;
-  username: string;
-  password: string;
+export class LoginComponent implements OnInit, OnDestroy {
+    hide = true;
+    login_sub: Subscription;
+    username: string;
+    password: string;
 
-  constructor(private authService: AuthService, private router: Router) { }
+    constructor(
+        private authService: AuthService,
+        private router: Router,
+        private snackBar: MatSnackBar
+    ) {
+    }
 
-  ngOnInit(): void {}
+    ngOnInit(): void {
+    }
 
-  ngOnDestroy(): void {
-    this.login_sub?.unsubscribe();
-  }
+    ngOnDestroy(): void {
+        this.login_sub?.unsubscribe();
+    }
 
-  onRegisterClick(): void {
-    this.router.navigate(['/register']);
-    console.log('register here redirected');
-  }
+    onRegisterClick(): void {
+        this.router.navigate(['/register']);
+        console.log('register here redirected');
+    }
 
-  login(user, pass): void {
-    this.login_sub = this.authService
-        .logIn(user, pass)
-        .subscribe(_ => {
-          this.router.navigateByUrl('/').then(r => location.reload());
-          }, console.error);
-  }
+    login(user, pass): void {
+        this.login_sub = this.authService
+            .logIn(user, pass)
+            .subscribe(response => {
+                    console.log(response.message);
+                    this.snackBar.open('Connexion réussie', 'OK', {
+                        duration: 2000
+                    });
+                }, error => {
+                    console.log(error.message);
+                    this.snackBar.open('Connexion échouée', 'OK', {
+                        duration: 2000, panelClass: ['mat-error']
+                    });
+                },
+                () => {
+                    this.router.navigateByUrl('/').then(r => location.reload());
+                }
+            );
+    }
 
-  onSubmit($event: Event): void {
-    this.login(this.username, this.password);
-  }
+    onSubmit($event: Event): void {
+        this.login(this.username, this.password);
+    }
 }
